d442cedb8df2e471e6fd47f8743c53018b0063f3
Kayleigh Ellis <elliskay@kayleighs-imac.home>
2022-04-09 12:14:23 +0100

    set up templates and pulling through data from mongo into the front end

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os 
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
     
     
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello, world!"
+@app.route("/get_properties")
+def get_properties():
+    properties = mongo.db.properties.find()
+    return render_template("properties.html", properties=properties)
 
 
 if __name__ == "__main__":
