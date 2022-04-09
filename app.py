f0649b864f9c29466a212233840c04a9cbc98bc1
Kayleigh Ellis <elliskay@Kayleighs-iMac.local>
2022-04-09 19:28:59 +0100

    set up registration for user checking for existing user and sending for data to mongo collection

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os 
 from flask import (
-    Flask, flash, render_template, 
-    redirect, session, url_for)
+    Flask, flash, render_template,
+    request, redirect, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -26,6 +26,24 @@ def get_properties():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        register = {
+            "username" : request.form.get("username").lower(), 
+            "password" : generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        
+        # puts the user into 'session' cookie
+        session['user'] = request.form.get("username").lower()
+        flash("Successfully registered")
     return render_template("register.html")
 
 
