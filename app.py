bad7f43d1533e45268fd9662b156f7d713381a1f
Kayleigh Ellis <elliskay@kayleighs-imac.home>
2022-04-14 00:40:02 +0100

    update and update_one is now deprecated replaced this with replace_one within the edit property function

--- app.py
+++ app.py
@@ -55,40 +55,45 @@ def login():
     if request.method == "POST":
         # checks if username exists in db
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-        
+            {"username": request.form.get("username").lower()}
+        )
+
         if existing_user:
             # ensure hashed password matches user inputed session
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Hey there, {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
-            else: 
+            existing_user["password"], request.form.get("password")
+            ):
+                session["user"] = request.form.get("username").lower()
+                flash("Hey there, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
+            else:
                 # password invalid match
-                flash(" Sorry that Username and/or Password didn't match anything in our records")
+                flash(
+                    " Sorry that Username and/or Password didn't match anything in our records"
+                )
                 return redirect(url_for("login"))
-            
+
         else:
             # username invalid match
-            flash("Sorry that Username and/or Password didn't match anything in our records")
-            return redirect(url_for('login'))
-        
+            flash(
+                "Sorry that Username and/or Password didn't match anything in our records"
+            )
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # gets the session user's username from the db
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-    
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+
     if session["user"]:
-        my_properties = mongo.db.properties.find({ "created_by": username })
-        return render_template("profile.html", username=username, my_properties=my_properties)
-    
+        my_properties = mongo.db.properties.find({"created_by": username})
+        return render_template(
+            "profile.html", username=username, my_properties=my_properties
+        )
+
     return redirect(url_for("login"))
 
 
@@ -103,18 +108,18 @@ def logout():
 @app.route("/list_property", methods=["GET", "POST"])
 def list_property():
     if request.method == "POST":
-       listing = {
-           "property_type": request.form.get("property_type"), 
-           "price": request.form.get("price"),
-           "city": request.form.get("city"),
-           "bedrooms": request.form.get("bedrooms"), 
-           "description": request.form.get("description"),
-           "img_url": request.form.getlist("img_url"),
-           "created_by": session["user"]
-       }
-       mongo.db.properties.insert_one(listing)
-       flash("Property Listing Successful!")
-       return redirect(url_for("get_properties"))
+        listing = {
+            "property_type": request.form.get("property_type"),
+            "price": request.form.get("price"),
+            "city": request.form.get("city"),
+            "bedrooms": request.form.get("bedrooms"),
+            "description": request.form.get("description"),
+            "img_url": request.form.getlist("img_url"),
+            "created_by": session["user"],
+        }
+        mongo.db.properties.insert_one(listing)
+        flash("Property Listing Successful!")
+        return redirect(url_for("get_properties"))
         # grabs every instance of property type, need to figure out how to aggregate
     property_type = list(mongo.db.type.find().sort("property_type", 1))
     return render_template("list_property.html", property_type=property_type)
@@ -122,10 +127,27 @@ def list_property():
 
 @app.route("/edit_property/<property_id>", methods=["GET", "POST"])
 def edit_property(property_id):
+    if request.method == "POST":
+        submit= {
+            "property_type": request.form.get("property_type"),
+            "price": request.form.get("price"),
+            "city": request.form.get("city"),
+            "bedrooms": request.form.get("bedrooms"),
+            "description": request.form.get("description"),
+            "img_url": request.form.getlist("img_url"),
+            "created_by": session["user"],
+        }
+        mongo.db.properties.replace_one({"_id": ObjectId(property_id)}, submit)
+        flash("Property Listing Updated!")
+    
     property = mongo.db.properties.find_one({"_id": ObjectId(property_id)})
     property_type = mongo.db.type.find().sort("property_type", 1)
-    return render_template("edit_property.html", property=property, property_type=property_type)
+    return render_template(
+        "edit_property.html", property=property, property_type=property_type
+    )
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
